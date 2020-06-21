34cba4f9767d8a4baf753843812a95fc3fff7bf0
soroushbahmani <soroush.bahmani.ir@gmail.com>
2020-06-21 02:02:03 -0700

    258

--- WebApplication1/App/app.js
+++ WebApplication1/App/app.js
@@ -1,4 +1,4 @@
-﻿//13:27
+﻿//13:30
 "use strict";
 define(['angularAMD', 'angular-ui-route', 'ui-bootstrap', 'angular-sanitize', 'ncy-angular-breadcrumb', 'blockUI', 'animate', 'moment', 'momentJalaali', 'infoWindow', 'preApp', 'dataService', 'helperFactory'],
     function (angularAMD) {
