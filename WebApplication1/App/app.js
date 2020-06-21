228f1e16a35f43a0a49bbfb3a01a1616f398fe5a
manyar98 <manyar.mohammadi98@gmail.com>
2020-06-21 13:29:27 +0430

    12345

--- WebApplication1/App/app.js
+++ WebApplication1/App/app.js
@@ -1,4 +1,4 @@
-﻿//13:23
+﻿//13:27
 "use strict";
 define(['angularAMD', 'angular-ui-route', 'ui-bootstrap', 'angular-sanitize', 'ncy-angular-breadcrumb', 'blockUI', 'animate', 'moment', 'momentJalaali', 'infoWindow', 'preApp', 'dataService', 'helperFactory'],
     function (angularAMD) {
