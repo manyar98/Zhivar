c5cd2155c8c0af6536079bcaa6a0e70bf7b2acff
omid <omid871@gmail.com>
2020-06-22 13:49:54 +0430

    550

--- WebApplication1/App/app.js
+++ WebApplication1/App/app.js
@@ -1,4 +1,4 @@
-﻿//500
+﻿//550
 "use strict";
 define(['angularAMD', 'angular-ui-route', 'ui-bootstrap', 'angular-sanitize', 'ncy-angular-breadcrumb', 'blockUI', 'animate', 'moment', 'momentJalaali', 'infoWindow', 'preApp', 'dataService', 'helperFactory'],
     function (angularAMD) {
